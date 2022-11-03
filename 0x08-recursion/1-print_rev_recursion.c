e3c866ee72d4faba01a98e90a49d6739b6881843
Precious-09 <adeogunprecious913@gmail.com>
2022-11-03 05:35:02 -0700

    Tasks

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,7 @@
-/**
- * File: 1-print_rev_recursion.c
- * Auth: sam
- */
-
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
+ * _print_rev_recursion - Prints a string in reverse
  * @s: The string to be printed
  */
 void _print_rev_recursion(char *s)
