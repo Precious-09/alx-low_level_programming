fb088c59fc72c3b7329e22005257e3c29af10e96
Precious-09 <adeogunprecious913@gmail.com>
2022-11-03 04:57:00 -0700

    Tasks

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,9 +1,11 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: char to be printed
  * Return: 1 on success
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
